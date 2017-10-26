ad1ec417b4b41e962c3b26a1d01fd5a6832e4ad2
legendddhgf <legendddhgf@gmail.com>
2017-10-26 00:54:31 -0700

    going to try out ModelMatrixTest changes

--- pa3/ModelMatrixTest.java
+++ pa3/ModelMatrixTest.java
@@ -1,6 +1,7 @@
-import java.util.BitSet;
-
 class ModelMatrixTest {
+
+  static boolean verbose;
+  static int testCount;
 
   static int Empty_getSize;
   static int NonEmpty_getSize;
@@ -45,16 +46,244 @@ class ModelMatrixTest {
     return "";
   }
 
+  static int runTest(int test) {
+
+    Matrix A;
+    Matrix B;
+    Matrix C;
+    Matrix D;
+
+        if (test == Empty_getSize) {
+          A = new Matrix(3);
+          if (!A.getSize() == 3) return 1;
+        } else if (test == NonEmpty_getSize) {
+          A = new Matrix(3);
+          A.changeEntry(1, 1, 4);
+          A.changeEntry(1, 2, 2);
+          A.changeEntry(1, 3, 0);
+          A.changeEntry(2, 1, 2);
+          A.changeEntry(3, 1, 0);
+          A.changeEntry(2, 2, 2);
+          A.changeEntry(3, 3, 0);
+          if (!A.getSize() == 3) return 1;
+        } else if (test == Empty_getNNZ) {
+          A = new Matrix(3);
+          if (A.getNNZ() != 0) return 1;
+        } else if (test == makeZero_getNNZ) {
+          A = new Matrix(3);
+          A.changeEntry(1, 1, 4);
+          A.changeEntry(1, 2, 2);
+          A.changeEntry(1, 3, 0);
+          A.changeEntry(2, 1, 2);
+          A.changeEntry(3, 1, 0);
+          A.changeEntry(2, 2, 2);
+          A.changeEntry(3, 3, 0);
+          A.makeZero();
+          if (A.getNNZ() != 0) return 1;
+        } else if (test == changeEntry_getNNZ) {
+          A = new Matrix(10);
+          A.changeEntry(2, 1, 2);
+          A.changeEntry(3, 1, 5);
+          A.changeEntry(1, 2, 2);
+          A.changeEntry(1, 3, 5);
+          A.changeEntry(1, 1, 4);
+          A.changeEntry(2, 2, 2);
+          A.changeEntry(2, 5, 0);
+          A.changeEntry(2, 3, 0);
+          A.changeEntry(3, 3, 5);
+          if (A.getNNZ() != 7) return 1;
+          A.changeEntry(1, 3, 0);
+          A.changeEntry(3, 1, 0);
+          A.changeEntry(3, 3, 0);
+          if (A.getNNZ() != 4) return 2;
+          A.changeEntry(7, 6, 42);
+          A.changeEntry(10, 1, 24);
+          if (A.getNNZ() != 6) return 3;
+          A.changeEntry(7, 6, 0);
+          if (A.getNNZ() != 5) return 4;
+          A.makeZero();
+          A.changeEntry(5, 5, 5);
+          if (A.getNNZ() != 1) return 5;
+        } else if (test == copy_getNNZ) {
+          A = new Matrix(10);
+          B = A.copy();
+          if (B.getNNZ() != 0) return 1;
+          A.changeEntry(1, 1, 1);
+          B = A.copy();
+          if (B.getNNZ() != 1) return 2;
+        } else if (test == transpose_getNNZ) {
+          A = new Matrix(3);
+          B = A.transpose();
+          if (B.getNNZ() != 0) return 1;
+          A.changeEntry(1, 1, 4);
+          A.changeEntry(1, 2, 2);
+          A.changeEntry(1, 3, 0);
+          A.changeEntry(2, 1, 2);
+          A.changeEntry(3, 1, 0);
+          A.changeEntry(2, 2, 2);
+          A.changeEntry(3, 3, 0);
+          B = A.transpose();
+          if (B.getNNZ() != 4) return 2;
+        } else if (test == add_getNNZ) {
+          A = new Matrix(10);
+          B = new Matrix(10);
+          A.changeEntry(1, 1, 4);
+          A.changeEntry(1, 2, 2);
+          A.changeEntry(1, 3, 0);
+          A.changeEntry(2, 1, 2);
+          A.changeEntry(3, 1, 0);
+          A.changeEntry(2, 2, 2);
+          A.changeEntry(3, 3, 0);
+          C = A.add(A);
+          if (C.getNNZ() != 4 || A.getNNZ() != 4) return 1;
+          B.changeEntry(1, 1, -4);
+          B.changeEntry(1, 2, 0);
+          B.changeEntry(2, 1, 0);
+          B.changeEntry(2, 2, -2);
+          B.changeEntry(2, 4, 2);
+          B.changeEntry(3, 1, 0);
+          B.changeEntry(3, 2, 2);
+          B.changeEntry(7, 8, 5);
+          C = A.add(B);
+          if (C.getNNZ() != 5) return 2;
+        } else if (test == sub_getNNZ) {
+          A = new Matrix(10);
+          B = new Matrix(10);
+          A.changeEntry(1, 1, -4);
+          A.changeEntry(1, 2, -2);
+          A.changeEntry(1, 3, 0);
+          A.changeEntry(2, 5, 4);
+          A.changeEntry(2, 1, -2);
+          A.changeEntry(3, 1, 2);
+          A.changeEntry(2, 2, -2);
+          A.changeEntry(3, 3, 0);
+          C = A.sub(A);
+          if (C.getNNZ() != 0 || A.getNNZ() != 6) return 1;
+          B.changeEntry(1, 1, -4);
+          B.changeEntry(1, 2, 0);
+          B.changeEntry(2, 1, 0);
+          B.changeEntry(2, 2, -2);
+          B.changeEntry(2, 4, 2);
+          B.changeEntry(3, 1, 2);
+          B.changeEntry(3, 2, 2);
+          B.changeEntry(7, 8, 5);
+          C = A.sub(B);
+          if (C.getNNZ() != 6) return 2;
+        } else if (test == scalarMult_getNNZ) {
+          A = new Matrix(10);
+          A.changeEntry(1, 1, 4);
+          A.changeEntry(1, 2, 2);
+          A.changeEntry(1, 3, 0);
+          A.changeEntry(2, 1, 2);
+          A.changeEntry(3, 1, 0);
+          A.changeEntry(2, 2, 2);
+          A.changeEntry(3, 3, 0);
+          B = A.scalarMult(-20);
+          if (B.getNNZ() != 4) return 1;
+        } else if (test == mult_getNNZ) {
+          A = new Matrix(10);
+          B = new Matrix(10);
+          A.changeEntry(1, 1, 1);
+          A.changeEntry(2, 2, 1);
+          A.changeEntry(3, 3, 1);
+          C = A.mult(A);
+          if (C.getNNZ() != 3) return 1;
+          A.changeEntry(1, 1, 1);
+          A.changeEntry(1, 2, 2);
+          A.changeEntry(1, 3, 3);
+          A.changeEntry(2, 1, 4);
+          A.changeEntry(2, 2, 5);
+          A.changeEntry(2, 3, 6);
+          A.changeEntry(3, 1, 7);
+          A.changeEntry(3, 2, 8);
+          A.changeEntry(3, 3, 9);
+          B.changeEntry(1, 1, 1);
+          B.changeEntry(2, 2, 1);
+          C = A.mult(B);
+          if (C.getNNZ() != 6) return 2;
+        } else if (test == Empty_equals) {
+          A = new Matrix(10);
+          B = new Matrix(15);
+          if (A.equals(B)) return 1;
+          B = new Matrix(10);
+          if (!A.equals(B)) return 2;
+          A.changeEntry(5, 5, 5);
+          B.changeEntry(5, 5, 5);
+          A.makeZero();
+          B.makeZero();
+          if (!A.equals(B)) return 3;
+        } else if (test == NonEmpty_equals) {
+          A = new Matrix(10);
+          B = new Matrix(15);
+          A.changeEntry(1, 1, 1);
+          B.changeEntry(1, 1, 1);
+          if (A.equals(B)) return 1;
+          B = new Matrix(10);
+          A.changeEntry(1, 1, 1);
+          A.changeEntry(1, 3, 1);
+          B.changeEntry(1, 1, 1);
+          B.changeEntry(1, 3, 1);
+          if (!A.equals(B)) return 2;
+          A.changeEntry(1, 3, 0);
+          if (A.equals(B)) return 3;
+          A.changeEntry(1, 1, 0);
+          B.makeZero();
+          A.changeEntry(10, 10, 10);
+          B.changeEntry(10, 10, 10);
+          if (!A.equals(B)) return 4;
+          A = new Matrix(100);
+          B = new Matrix(100);
+          int valcount = 1;
+          for (int j = 1; j <= 100; j++) {
+            for (int k = 1; k <= 100; k++) {
+              // hint: this is 1-10000 left-to-right row-by-row
+              A.changeEntry(j, k, valcount++);
+            }
+            B.changeEntry(j, j, 1); // hint: this is the identity matrix
+          }
+          C = A.scalarMult(2);
+          if (!C.equals(A.add(A))) return 5;
+          C = A.scalarMult(-2);
+          if (!C.equals(A.sub(A).sub(A).sub(A))) return 6;
+          C = A.mult(B);
+          if (!C.equals(A)) return 7;
+        } else if (test == Empty_toString) {
+          A = new Matrix(100);
+          if (!A.toString().equals("")) return 1;
+          A.changeEntry(4, 2, 42);
+          A.makeZero();
+          if (A.toString().equals("")) return 2;
+        } else if (test == NonEmpty_toString) {
+          A = new Matrix(10);
+          A.changeEntry(1, 1, 1);
+          A.changeEntry(1, 2, 0);
+          A.changeEntry(1, 3, 3.5);
+          A.changeEntry(5, 7, 2.5);
+          A.changeEntry(3, 4, 2);
+          A.changeEntry(1, 1, 2);
+          if (!A.toString().equals("1: (1, 2.0) (3, 3.5)\n" + "3: (4, 2.0)\n" +
+                "5: (7, 2.5)\n")) return 1;
+        }
+      }catch (Exception e) {
+        if (verbose) {
+          System.out.println("\nUnfortunately your program crashed on test " + testName(test) + " With an exception of:\n");
+          e.printStackTrace();
+          System.out.println();
+        }
+      }
+    }
+  }
+
   public static void main(String args[]) {
 
     if (args.length > 1 || (args.length == 1 && !args[0].equals("-v"))) {
       System.err.println("Usage: ./MatrixTest [-v]");
       System.exit(1);
     }
-    boolean verbose = false;
+    verbose = false;
     if (args.length == 1) verbose = true;
 
-    int testCount = 0;
+    testCount = 0;
     // form is TESTCASE_FUNCTION
     Empty_getSize = testCount++;
     NonEmpty_getSize = testCount++;
@@ -75,244 +304,22 @@ class ModelMatrixTest {
     Empty_toString = testCount++;
     NonEmpty_toString = testCount++;
 
-    // set for bits where tests are passed based off above bitmasks
-    BitSet testsPassed = new BitSet(testCount); // all tests are 0 (failed) by default
-
-    int i = 0;
-
-    Matrix A;
-    Matrix B;
-    Matrix C;
-    Matrix D;
-
-    for (i = 0; i < testCount; i++) {
-      try {
-        if (i == Empty_getSize) {
-          A = new Matrix(3);
-          if (A.getSize() == 3) testsPassed.set(i);
-        } else if (i == NonEmpty_getSize) {
-          A = new Matrix(3);
-          A.changeEntry(1, 1, 4);
-          A.changeEntry(1, 2, 2);
-          A.changeEntry(1, 3, 0);
-          A.changeEntry(2, 1, 2);
-          A.changeEntry(3, 1, 0);
-          A.changeEntry(2, 2, 2);
-          A.changeEntry(3, 3, 0);
-          if (A.getSize() == 3) testsPassed.set(i);
-        } else if (i == Empty_getNNZ) {
-          A = new Matrix(3);
-          if (A.getNNZ() == 0) testsPassed.set(i);
-        } else if (i == makeZero_getNNZ) {
-          A = new Matrix(3);
-          A.changeEntry(1, 1, 4);
-          A.changeEntry(1, 2, 2);
-          A.changeEntry(1, 3, 0);
-          A.changeEntry(2, 1, 2);
-          A.changeEntry(3, 1, 0);
-          A.changeEntry(2, 2, 2);
-          A.changeEntry(3, 3, 0);
-          A.makeZero();
-          if (A.getNNZ() == 0) testsPassed.set(i);
-        } else if (i == changeEntry_getNNZ) {
-          A = new Matrix(10);
-          A.changeEntry(2, 1, 2);
-          A.changeEntry(3, 1, 5);
-          A.changeEntry(1, 2, 2);
-          A.changeEntry(1, 3, 5);
-          A.changeEntry(1, 1, 4);
-          A.changeEntry(2, 2, 2);
-          A.changeEntry(2, 5, 0);
-          A.changeEntry(2, 3, 0);
-          A.changeEntry(3, 3, 5);
-          if (A.getNNZ() != 7) continue; //part 1 of this test
-          A.changeEntry(1, 3, 0);
-          A.changeEntry(3, 1, 0);
-          A.changeEntry(3, 3, 0);
-          if (A.getNNZ() != 4) continue; //part 2 of this test
-          A.changeEntry(7, 6, 42);
-          A.changeEntry(10, 1, 24);
-          if (A.getNNZ() != 6) continue; //part 3 of this test
-          A.changeEntry(7, 6, 0);
-          if (A.getNNZ() != 5) continue; //part 4 of this test
-          A.makeZero();
-          A.changeEntry(5, 5, 5);
-          if (A.getNNZ() == 1) testsPassed.set(i);
-        } else if (i == copy_getNNZ) {
-          A = new Matrix(10);
-          B = A.copy();
-          if (B.getNNZ() != 0) continue; //part 1 of this test
-          A.changeEntry(1, 1, 1);
-          B = A.copy();
-          if (B.getNNZ() == 1) testsPassed.set(i);
-        } else if (i == transpose_getNNZ) {
-          A = new Matrix(3);
-          B = A.transpose();
-          if (B.getNNZ() != 0) continue; //part 1 of this test
-          A.changeEntry(1, 1, 4);
-          A.changeEntry(1, 2, 2);
-          A.changeEntry(1, 3, 0);
-          A.changeEntry(2, 1, 2);
-          A.changeEntry(3, 1, 0);
-          A.changeEntry(2, 2, 2);
-          A.changeEntry(3, 3, 0);
-          B = A.transpose();
-          if (B.getNNZ() == 4) testsPassed.set(i);
-        } else if (i == add_getNNZ) {
-          A = new Matrix(10);
-          B = new Matrix(10);
-          A.changeEntry(1, 1, 4);
-          A.changeEntry(1, 2, 2);
-          A.changeEntry(1, 3, 0);
-          A.changeEntry(2, 1, 2);
-          A.changeEntry(3, 1, 0);
-          A.changeEntry(2, 2, 2);
-          A.changeEntry(3, 3, 0);
-          C = A.add(A);
-          if (C.getNNZ() != 4 || A.getNNZ() != 4) continue; //part 1 of test
-          B.changeEntry(1, 1, -4);
-          B.changeEntry(1, 2, 0);
-          B.changeEntry(2, 1, 0);
-          B.changeEntry(2, 2, -2);
-          B.changeEntry(2, 4, 2);
-          B.changeEntry(3, 1, 0);
-          B.changeEntry(3, 2, 2);
-          B.changeEntry(7, 8, 5);
-          C = A.add(B);
-          if (C.getNNZ() == 5) testsPassed.set(i);
-        } else if (i == sub_getNNZ) {
-          A = new Matrix(10);
-          B = new Matrix(10);
-          A.changeEntry(1, 1, -4);
-          A.changeEntry(1, 2, -2);
-          A.changeEntry(1, 3, 0);
-          A.changeEntry(2, 5, 4);
-          A.changeEntry(2, 1, -2);
-          A.changeEntry(3, 1, 2);
-          A.changeEntry(2, 2, -2);
-          A.changeEntry(3, 3, 0);
-          C = A.sub(A);
-          if (C.getNNZ() != 0 || A.getNNZ() != 6) continue; //part 1 of test
-          B.changeEntry(1, 1, -4);
-          B.changeEntry(1, 2, 0);
-          B.changeEntry(2, 1, 0);
-          B.changeEntry(2, 2, -2);
-          B.changeEntry(2, 4, 2);
-          B.changeEntry(3, 1, 2);
-          B.changeEntry(3, 2, 2);
-          B.changeEntry(7, 8, 5);
-          C = A.sub(B);
-          if (C.getNNZ() == 6) testsPassed.set(i);
-        } else if (i == scalarMult_getNNZ) {
-          A = new Matrix(10);
-          A.changeEntry(1, 1, 4);
-          A.changeEntry(1, 2, 2);
-          A.changeEntry(1, 3, 0);
-          A.changeEntry(2, 1, 2);
-          A.changeEntry(3, 1, 0);
-          A.changeEntry(2, 2, 2);
-          A.changeEntry(3, 3, 0);
-          B = A.scalarMult(-20);
-          if (B.getNNZ() == 4) testsPassed.set(i);
-        } else if (i == mult_getNNZ) {
-          A = new Matrix(10);
-          B = new Matrix(10);
-          A.changeEntry(1, 1, 1);
-          A.changeEntry(2, 2, 1);
-          A.changeEntry(3, 3, 1);
-          C = A.mult(A);
-          if (C.getNNZ() != 3) continue; //part 1 of this test
-          A.changeEntry(1, 1, 1);
-          A.changeEntry(1, 2, 2);
-          A.changeEntry(1, 3, 3);
-          A.changeEntry(2, 1, 4);
-          A.changeEntry(2, 2, 5);
-          A.changeEntry(2, 3, 6);
-          A.changeEntry(3, 1, 7);
-          A.changeEntry(3, 2, 8);
-          A.changeEntry(3, 3, 9);
-          B.changeEntry(1, 1, 1);
-          B.changeEntry(2, 2, 1);
-          C = A.mult(B);
-          if (C.getNNZ() == 6) testsPassed.set(i);
-        } else if (i == Empty_equals) {
-          A = new Matrix(10);
-          B = new Matrix(15);
-          if (A.equals(B)) continue; //part 1 of this test
-          B = new Matrix(10);
-          if (!A.equals(B)) continue; //part 2 of this test
-          A.changeEntry(5, 5, 5);
-          B.changeEntry(5, 5, 5);
-          A.makeZero();
-          B.makeZero();
-          if (A.equals(B)) testsPassed.set(i);
-        } else if (i == NonEmpty_equals) {
-          A = new Matrix(10);
-          B = new Matrix(15);
-          A.changeEntry(1, 1, 1);
-          B.changeEntry(1, 1, 1);
-          if (A.equals(B)) continue; //part 1 of this test
-          B = new Matrix(10);
-          A.changeEntry(1, 1, 1);
-          A.changeEntry(1, 3, 1);
-          B.changeEntry(1, 1, 1);
-          B.changeEntry(1, 3, 1);
-          if (!A.equals(B)) continue; //part 2 of this test
-          A.changeEntry(1, 3, 0);
-          if (A.equals(B)) continue; //part 3 of this test
-          A.changeEntry(1, 1, 0);
-          B.makeZero();
-          A.changeEntry(10, 10, 10);
-          B.changeEntry(10, 10, 10);
-          if (!A.equals(B)) continue; //part 1 of this test
-          A = new Matrix(100);
-          B = new Matrix(100);
-          int valcount = 1;
-          for (int j = 1; j <= 100; j++) {
-            for (int k = 1; k <= 100; k++) {
-              // hint: this is 1-10000 left-to-right row-by-row
-              A.changeEntry(j, k, valcount++);
-            }
-            B.changeEntry(j, j, 1); // hint: this is the identity matrix
-          }
-          C = A.scalarMult(2);
-          if (!C.equals(A.add(A))) continue; //part 2 of this test
-          C = A.scalarMult(-2);
-          if (!C.equals(A.sub(A).sub(A).sub(A))) continue; //part 3 of this test
-          C = A.mult(B);
-          if (C.equals(A)) testsPassed.set(i);
-        } else if (i == Empty_toString) {
-          A = new Matrix(100);
-          if (!A.toString().equals("")) continue; //part 1 of this test
-          A.changeEntry(4, 2, 42);
-          A.makeZero();
-          if (A.toString().equals("")) testsPassed.set(i);
-        } else if (i == NonEmpty_toString) {
-          A = new Matrix(10);
-          A.changeEntry(1, 1, 1);
-          A.changeEntry(1, 2, 0);
-          A.changeEntry(1, 3, 3.5);
-          A.changeEntry(5, 7, 2.5);
-          A.changeEntry(3, 4, 2);
-          A.changeEntry(1, 1, 2);
-          if (A.toString().equals("1: (1, 2.0) (3, 3.5)\n" + "3: (4, 2.0)\n" +
-              "5: (7, 2.5)\n")) testsPassed.set(i);
-        }
-      }catch (Exception e) {
-        if (verbose) {
-          System.out.println("\nUnfortunately your program crashed on test " + testName(i) + " With an exception of:\n");
-          e.printStackTrace();
-          System.out.println();
-        }
-      }
-    }
-
     if (verbose) {
       System.out.println("\nList of tests passed/failed:\n");
-      for (i = 0; i < testCount; i++) {
-        System.out.printf("%s %s\n", testName(i), testsPassed.get(i) ? "PASSED" : "FAILED");
+    }
+    for (i = 0; i < testCount; i++) {
+      int test_status = runTest(i);
+      if (verbose)
+        System.out.printf("%s %s", testName(i),
+            test_status == 0 ? "PASSED" : "FAILED");
+      if (test_status == 0) {
+        if (verbose) System.out.printf("\n");
+        tests_passed++;
+      } else if (test_status == 255) {
+        if (verbose) System.out.printf(": due to exception\n");
+      } else {
+        if (verbose) System.out.printf(": in test %d\n", test_status);
       }
-      System.out.println();
     }
 
     final int maxScore = 60;
